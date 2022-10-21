534edd3f6ec2fd4988936e27aa400c6f8e1f9cfa
who(true) <alexkjh2011@gmail.com>
2022-10-22 02:48:18 +0900

    Information Hiding  in UseModel class

--- src/main/java/com/example/project/UserModel.java
+++ src/main/java/com/example/project/UserModel.java
@@ -5,10 +5,12 @@ import lombok.*;
 @Getter
 @Setter
 @AllArgsConstructor
-public class UserModel {
-
+@NoArgsConstructor
+public class UserModel { //단순하게 인변만 넣은거
+    //자바 은닉화 or 캡슐화
     private String id;
     private String nickname;
     private String password;
+
 
 }
