5a0d84f5e5fe33c6756fe6964622f2b606dff73f
who(true) <alexkjh2011@gmail.com>
2022-10-22 02:50:04 +0900

    use DI in UserRepository and make public UserModel method

--- src/main/java/com/example/project/UserRepository.java
+++ src/main/java/com/example/project/UserRepository.java
@@ -1,17 +1,23 @@
 package com.example.project;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowCallbackHandler;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 
 @Repository
 public class UserRepository {
 
-    @Autowired
-    JdbcTemplate jdbcTemplate;
+   private final JdbcTemplate jdbcTemplate;
+
+    public UserRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     public boolean addUser(String id){
         jdbcTemplate.execute("INSERT INTO user (id) VALUES ('" + id + "')");
@@ -24,11 +30,23 @@ public class UserRepository {
         return true;
     }
 
-
-
-
     public boolean checkUser(String id){
         Integer count = jdbcTemplate.queryForObject("SELECT count(*) FROM user WHERE id = '" + id + "'", Integer.class);
         return count == 0;
     }
+
+    public UserModel getUser(String id){
+        UserModel userModel = new UserModel();
+        jdbcTemplate.query("SELECT * FROM user WHERE id = '" + id + "'", new RowCallbackHandler() {
+            @Override
+            public void processRow(ResultSet rs) throws SQLException, DataAccessException {
+                userModel.setId(rs.getString("id"));
+                userModel.setNickname(rs.getString("nickname"));
+                userModel.setPassword( rs.getString("password"));
+            }
+        });
+        return userModel;
+
+    }
+
 }
