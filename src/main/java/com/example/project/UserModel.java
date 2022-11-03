7fc1224db80f4ec5dc85442fadc13ad14974c9e2
김민우 <rlaalsdn4564@hanmail.net>
2022-11-03 23:36:18 +0900

    Update UserModel.java

--- src/main/java/com/example/project/UserModel.java
+++ src/main/java/com/example/project/UserModel.java
@@ -7,7 +7,7 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserModel { //단순하게 인변만 넣은거
-    //자바 은닉화 or 캡슐화
+    //자바 은닉화 or 캡슐화!
     private String id;
     private String nickname;
     private String password;
